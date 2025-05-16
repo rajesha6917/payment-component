3a6a666f8880cb28e70552c9e9c20abef1474c63
Ubuntu <Rajesh@MACH.bnprm2lsoklexipleihqu5r4db.rx.internal.cloudapp.net>
2025-05-16 15:19:18 +0000

    Add terraform files for static website component

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  is_hns_enabled           = false
+  static_website {
+    index_document = "index.html"
+    error_404_document = "404.html"
+  }
+}
+
+resource "azurerm_storage_blob" "index" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.storage.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  source_content         = "<html><body><h1>Hello from MACH Composer POC!</h1></body></html>"
+}
---
+++ terraform/output.tf
@@ -0,0 +1,3 @@
+output "static_website_url" {
+  value = azurerm_storage_account.storage.primary_web_endpoint
+}
